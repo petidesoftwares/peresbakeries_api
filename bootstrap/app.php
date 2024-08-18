a24d93d5ece1bf0d052cdb92c4f109cf90d91ea6
petidesoftwares <petideenterprise@gmail.com>
2024-08-18 20:48:57 +0100

    cors.php add and registered

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(['assign.guard'=>AssignGuard::class]);
+        $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
