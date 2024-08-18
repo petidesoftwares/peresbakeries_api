c54f61dcf332122f68a834d3adb0862510db5f62
petidesoftwares <petideenterprise@gmail.com>
2024-08-18 21:26:21 +0100

    cors updated

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
          $response = $next($request);
 
-         $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
         $response->header->set('support_credentials', true);
