2dfa0de52e13ff4ce6cdc69e7c116e55dacd6829
petidesoftwares <petideenterprise@gmail.com>
2024-08-18 22:17:50 +0100

    App.php updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AssignGuard;
+use App\Http\Middleware\Cors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
