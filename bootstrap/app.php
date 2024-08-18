6abdafb33c6f5729648c7923b7257424bf30ff5d
petidesoftwares <petideenterprise@gmail.com>
2024-08-18 22:14:08 +0100

    app updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(['assign.guard'=>AssignGuard::class]);
-        $middleware->pushMiddleware(Cors::class);
+        $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
