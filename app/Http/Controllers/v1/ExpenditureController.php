97d6cf83e19e97dc2ce50843c6119e6bb3fe7c4c
petidesoftwares <petideenterprise@gmail.com>
2024-08-20 08:10:50 +0100

    namespace added

--- app/Http/Controllers/v1/ExpenditureController.php
+++ app/Http/Controllers/v1/ExpenditureController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Expenditure;
 
 class ExpenditureController extends Controller
 {
