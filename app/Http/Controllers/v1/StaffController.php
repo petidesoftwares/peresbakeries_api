b21d52a8121630adda993ead00e7d48253a51732
petidesoftwares <petideenterprise@gmail.com>
2024-08-22 21:30:20 +0100

    Update StaffController.php

--- app/Http/Controllers/v1/StaffController.php
+++ app/Http/Controllers/v1/StaffController.php
@@ -92,7 +92,7 @@ class StaffController extends Controller
     public function store(Request $request)
     {
         $user = Auth::user();
-        // if($user->position == "CEO"){
+        if($user->position == "CEO"){
             $validator = Validator::make([
                 'firstname'=>$request->input('firstname'),
                 'surname'=>$request->input('surname'),
@@ -113,7 +113,7 @@ class StaffController extends Controller
     
             $validator->validate();
             
-            // if($request->input('position') != "CEO"){
+            if($request->input('position') != "CEO"){
                 $checkManager = Staff::where("position","Manager")->first();
                 if($request->input('position') == "Manager" && !empty($checkManager)){
                     return response()->json(['status'=>300, "message"=>"Manager already exist"],300);
@@ -132,10 +132,10 @@ class StaffController extends Controller
                 $created = Staff::create($staff);
         
                 return response()->json(['status'=>200, "data"=> $created, 'message'=>'Staff created successfuly.'],200);
-            // }
-            // return response()->json(["status"=>300, "message"=>"Position already taken"],300);    
+            }
+            return response()->json(["status"=>300, "message"=>"Position already taken"],300);    
             
-        // }
+        }
         
 
     }
