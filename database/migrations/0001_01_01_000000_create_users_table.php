757fbcc5a56b3af4a0035b8e7c133d03acc1fc1f
petidesoftwares <petideenterprise@gmail.com>
2024-08-23 14:54:23 +0100

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -29,11 +29,11 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->foreignUuid('staff_id')->constrained();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            // $table->longText('payload');
+            // $table->integer('last_activity')->index();
         });
     }
 
