20a07a6529a553d0e0c1ad9c7e78bc54336e3d6c
petidesoftwares <petideenterprise@gmail.com>
2024-08-23 16:30:47 +0100

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Staff;
 
 class LoginController extends AuthController
 {
