148802b44b2924371d501b4d3bb1bff08498e026
petidesoftwares <petideenterprise@gmail.com>
2024-08-26 10:17:03 +0100

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -84,8 +84,8 @@ class LoginController extends AuthController
         ];
 
         $user = Staff::where("mobile_number",$credentials['mobile_number'])->get("id");
-        $session = DB::table("sessions")->select("staff_id")->get();
-        return count($session) ;//> 0 && $user[0]->id == $session[0]->staff_id;
+        $session = DB::select('select staff_id from sessions where staff_id =?', [$user->id]);
+        return response()->json(["session"=>$session]);
         if(count($session) > 0 && $user[0]->id == $session[0]->staff_id){
             return response()->json(['status'=>401, "message"=>"This user already logged"]);
         }
