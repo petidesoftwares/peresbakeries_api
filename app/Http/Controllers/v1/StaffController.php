8291bffcd21bde62ca1707a8ca6a64ea4c4f3824
petidesoftwares <petideenterprise@gmail.com>
2024-08-27 21:05:41 +0100

    Update StaffController.php

--- app/Http/Controllers/v1/StaffController.php
+++ app/Http/Controllers/v1/StaffController.php
@@ -228,12 +228,10 @@ class StaffController extends Controller
         if($request->input("mobile_number") != null && $request->input("mobile_number") != ""){
             $request->validate(["mobile_number"=>"min:11|regex:/^([0-9\s\-\+\(\)]*)$/|unique:staff,mobile_number"]);
         }
-        return $editedData;
 
         $updater = Staff::where("id", $id)->update(["gender" => $editedData->gender]);
-        return $updater;
 
-        return response()->json(["status"=>200, "message"=>"Staff successfully updated"],200);
+        return response()->json(["status"=>200, "data" =>$updater, "message"=>"Staff successfully updated"],200);
     }
 
         /**
