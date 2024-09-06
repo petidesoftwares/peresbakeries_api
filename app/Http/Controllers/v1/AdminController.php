d88d3b44314144c31f78b9d819251a5ceccf064d
petidesoftwares <petideenterprise@gmail.com>
2024-09-06 21:25:56 +0100

    Update AdminController.php

--- app/Http/Controllers/v1/AdminController.php
+++ app/Http/Controllers/v1/AdminController.php
@@ -20,20 +20,32 @@ class AdminController extends Controller
         $cashSales = Sales::where("payment_method","=","Cash")->where('created_at','like',$date."%")->get('amount');
         $expenditure = Expenditure::where("created_at","like",$date."%")->get("amount");
         $util = new Util();
+        $totalCashSales = 0;
+        $totalBankSales = 0;
+        $totalExpenses = 0;
         $motherArray = ["date"=>$date,];
         if(count($cashSales) >0){
             $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
             $motherArray["cash_sales"] =$cashSales;
+            foreach($cashSales as $sales_1){
+                $totalCashSales += $sales_1->amount;
+            }
         }
         if(count($bankSales) > 0){
             $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
             $motherArray["bank_sales"] = $bankSales;
+            foreach($bankSales as $sales_2){
+                $totalBankSales += $sales_2->amount;
+            }
         }
         if(count($expenditure) > 0){
             $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
             $motherArray["expenses"] = $expenditure;
+            foreach($expenditure as $expense){
+                $totalExpenses += $expense->amount;
+            }
         }
-        return $motherArray;
+        return response()->json(["totalCash"=>$totalCashSales, "totalBank"=>$totalBankSales, "totalExp"=>$totalExpenses]);
         $pdf = Pdf::loadView('dailycashbook',compact('motherArray'))->setPaper('a4','landscape');
         return $pdf->download('Daily Cashbook.pdf');
     }
