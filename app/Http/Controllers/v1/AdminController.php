a0c71272671d77f4fe69562e8ede2ebad42c0639
petidesoftwares <petideenterprise@gmail.com>
2024-09-18 21:56:27 +0100

    Update AdminController.php

--- app/Http/Controllers/v1/AdminController.php
+++ app/Http/Controllers/v1/AdminController.php
@@ -24,31 +24,36 @@ class AdminController extends Controller
         $totalBankSales = 0;
         $totalExpenses = 0;
         $motherArray = ["date"=>$date,];
-        if(count($cashSales) >0){
-            $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
-            $motherArray["cash_sales"] =$cashSales;
-            foreach($cashSales as $sales_1){
-                $totalCashSales += $sales_1->amount;
+        if(count($cashSales) >0 || count($bankSales) > 0 || count($expenditure) > 0){
+            if(count($cashSales) >0){
+                $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
+                $motherArray["cash_sales"] =$cashSales;
+                foreach($cashSales as $sales_1){
+                    $totalCashSales += $sales_1->amount;
+                }
+                $motherArray["total_cash_sales"] = $totalCashSales;
             }
-            $motherArray["total_cash_sales"] = $totalCashSales;
-        }
-        if(count($bankSales) > 0){
-            $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
-            $motherArray["bank_sales"] = $bankSales;
-            foreach($bankSales as $sales_2){
-                $totalBankSales += $sales_2->amount;
+            if(count($bankSales) > 0){
+                $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
+                $motherArray["bank_sales"] = $bankSales;
+                foreach($bankSales as $sales_2){
+                    $totalBankSales += $sales_2->amount;
+                }
+                $motherArray["total_bank_sales"] = $totalBankSales;
             }
-            $motherArray["total_bank_sales"] = $totalBankSales;
-        }
-        if(count($expenditure) > 0){
-            $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
-            $motherArray["expenses"] = $expenditure;
-            foreach($expenditure as $expense){
-                $totalExpenses += $expense->amount;
+            if(count($expenditure) > 0){
+                $motherArray["size"] = $util->findLargestArraySize($cashSales, $bankSales, $expenditure);
+                $motherArray["expenses"] = $expenditure;
+                foreach($expenditure as $expense){
+                    $totalExpenses += $expense->amount;
+                }
             }
+            $motherArray["total_expenses"] = $totalExpenses;
+            $pdf = Pdf::loadView('dailycashbook',compact('motherArray'))->setPaper('a4','landscape');
+            return $pdf->download('Daily Cashbook.pdf');
+        }else{
+            return response()->json(['status'=>200, 'message' => "No sales Sales or expenditure today"]);
         }
-        $motherArray["total_expenses"] = $totalExpenses;
-        $pdf = Pdf::loadView('dailycashbook',compact('motherArray'))->setPaper('a4','landscape');
-        return $pdf->download('Daily Cashbook.pdf');
+        
     }
 }
